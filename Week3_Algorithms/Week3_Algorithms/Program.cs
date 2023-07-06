83da6d3f02c01130e67c8d7c76837a34919658b4
Fleșariu Ștefana-Otilia <stefanaflesariu@yahoo.com>
2023-07-06 22:46:20 +0200

    Week3_algorithms

---
+++ Week3_Algorithms/Week3_Algorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿//Bubble Sort
+int[] array = new int[]  { 2, 5, 8, 12, 16, 38, 23, 56, 72, 91 };
+int temp=0;
+//for(int i = 0; i < array.Length; i++)
+//{
+//    for (int j = 0; j < array.Length-1; j++)
+//    {
+//        if (array[j] > array[j + 1])
+//        {
+//            temp = array[j];
+//            array[j] = array[j + 1];
+//            array[j + 1] = temp; 
+//        }
+//    }
+
+//}
+//int k;
+//for (k = 0; k < array.Length; k++)
+//{
+//    Console.Write(array[k] + " ");
+//}
+
