6f03c1428066136117249138e920d2d720bcdeef
Fleșariu Ștefana-Otilia <stefanaflesariu@yahoo.com>
2023-07-16 20:04:15 +0200

    an added exercise

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *  Write a C# program to check two given numbers where one is less than 100 and the other is greater than 200.
+    Sample Output:
+    Input a first number(<100): 75
+    Input a second number(>100): 250
+    True
+*/
+
+Console.Write("Input a first number(<100) : ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Input a  second number(>100) : ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine((firstNumber < 100 && secondNumber > 100));
+
+
+
+
