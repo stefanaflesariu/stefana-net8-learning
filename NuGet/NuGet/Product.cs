b17cf10d08f7bb851a2d2f4c487a3ee680775a57
Fleșariu Ștefana-Otilia <stefanaflesariu@yahoo.com>
2023-08-17 14:54:36 +0200

    NuGet_Homework

---
+++ NuGet/NuGet/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
