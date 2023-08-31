7aecf833eaba36c2eaba73f40c6bfe6e3e00710a
Fleșariu Ștefana-Otilia <stefanaflesariu@yahoo.com>
2023-08-31 21:23:36 +0200

    Exercise2

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -9,7 +9,7 @@ namespace Homework
         {
             //Exercise 1
             Console.WriteLine("Reversed list of elements of type int");
-            List<int> list = new List<int>() { 1, 8, 6, 9, 3, 4 };
+            List<int> list = new List<int>() { 1, 8, 6, 9, 3, 4, 1, 6, 3 };
             ReverseOrderOflements<int>(list);
 
             Console.WriteLine();
@@ -17,8 +17,13 @@ namespace Homework
             List<string> list1 = new List<string>() { "A", "B", "D", "M", "F" };
             ReverseOrderOflements<string>(list1);
 
+            Console.WriteLine();
             //Exercise 2
+            DisplayDistinctValues(list);
+            Console.WriteLine();
+            DisplayDistinctValues(list1);
 
+            //
         }
 
         // 1.Create a generic method that takes in a list of any type and reverses the order of the elements.
@@ -34,11 +39,16 @@ namespace Homework
                 Console.Write(i + " ");
             }
         }
+
         // 2.Create a generic method that takes in a list of any type and applies a specific action to each element
         // using a lambda expression.
-        public static void Expresion<T>(List<T> list)
+        public static void DisplayDistinctValues<T>(List<T> list)
         {
-
+            var list2 = list.Select(x => x + " ana");
+            foreach (var i in list2)
+            {
+                Console.Write(i + " ");
+            }
         }
 
     }
